8c73a9a5498337805000de5fa5e804007386bf14
Ayushhh <89790082+Graffian@users.noreply.github.com>
2024-03-04 19:31:54 +0530

    main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let secondEl = document.getElementById("seconds");
 let hoursEl = document.getElementById("hours");
 let minutesEl = document.getElementById("minutes");
+let millisecondEl= document.getElementById("milliseconds")
 let n = parseInt(secondEl.innerHTML);
 let k = parseInt(minutesEl.innerHTML);
 let c = parseInt(hoursEl.innerHTML);
+let d=parseInt(millisecondEl.innerHTML)
 let updateSecondsInterval
 let updateMinutesInterval
 let updateHoursInterval
@@ -27,13 +29,33 @@ function updateHours() {
   c = c + 1;
   hoursEl.innerHTML = c;
 }
+function updateMilliSecond(){
+  d=d+1
+  millisecondEl.innerHTML=d
+  if(d>=100){
+    d=0
+    millisecondEl.innerHTML=d
+  }
+}
 function startBtn(){
+  updateMilliSecondInterval=setInterval(updateMilliSecond,10)
 updateSecondsInterval= setInterval(updateSeconds, 1000);
 updateMinutesInterval= setInterval(updateMinutes, 60000);
 updateHoursInterval= setInterval(updateHours, 3600000);
 }
 function stopBtn(){
+  clearInterval(updateMilliSecondInterval)
   clearInterval(updateSecondsInterval)
   clearInterval(updateMinutesInterval)
   clearInterval(updateHoursInterval)
+}
+function resetBtn(){
+  n = 0;
+    secondEl.innerHTML = n;
+    d=0
+    millisecondEl.innerHTML=d
+    k = 0;
+    minutesEl.innerHTML = k;
+    c=0
+    hoursEl.innerHTML = c;
 }
